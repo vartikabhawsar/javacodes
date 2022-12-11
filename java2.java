dbb38bbab13738a2b8ffffd6c88fcdede4d0e531
vartikabhawsar <98348633+vartikabhawsar@users.noreply.github.com>
2022-12-11 23:51:38 +0530

    Add files via upload

---
+++ java2.java
@@ -0,0 +1,10 @@
+public class Main  
+ {  
+   public static void main (String args[])  
+    {   float p, r,  t,  si; 
+              p = 13000 ;  r = 12; t = 2;  
+               si  = (p*r*t )/100;   
+              System.out.println("Simple Interest is: " +si);  
+    }
+     
+ }  
