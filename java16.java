20e596242277c64e24dd94fc5903b93506524545
vartikabhawsar <98348633+vartikabhawsar@users.noreply.github.com>
2023-01-07 23:55:08 +0530

    Add files via upload

---
+++ java16.java
@@ -0,0 +1,19 @@
+class Student
+{
+    String name;
+    int rn;
+    Student (String n,int r)
+    {
+        name=n;
+        rn=r;
+    }
+   
+}
+class Main
+{
+     public static void main (String[] args) {
+        Student s1= new Student("Vartika", 41);
+        System.out.println(s1);
+     }
+  
+}
